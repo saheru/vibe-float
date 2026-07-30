ebf75b139ce4aee4a6fe4e6a9a58050d2a63173b
saheru <8498471+saheru@users.noreply.github.com>
2026-07-30 16:40:40 +0800

    design: make app icon reflect Codex Float dashboard

--- scripts/build-codex-float.sh
+++ scripts/build-codex-float.sh
@@ -33,8 +33,8 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
   <key>CFBundleDisplayName</key><string>Codex Float</string>
   <key>CFBundleIconFile</key><string>CodexFloat</string>
   <key>CFBundlePackageType</key><string>APPL</string>
-  <key>CFBundleShortVersionString</key><string>0.2.2</string>
-  <key>CFBundleVersion</key><string>6</string>
+  <key>CFBundleShortVersionString</key><string>0.2.3</string>
+  <key>CFBundleVersion</key><string>7</string>
   <key>LSMinimumSystemVersion</key><string>14.0</string>
   <key>LSUIElement</key><true/>
   <key>NSHighResolutionCapable</key><true/>
